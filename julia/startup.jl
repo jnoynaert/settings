290e5c938d02a086102cf26bf3f08613f3d864f0
Jared <jmnoynaert@gmail.com>
2020-01-10 14:42:05 -0600

    added  fn to Julia startup

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,7 +1,26 @@
 using REPL
 
 atreplinit() do repl
-    if isdefined(repl, :options)
-        repl.options = REPL.Options(auto_indent=false)
+    # auto-load Revise.jl
+    #=
+    try
+        @eval using Pkg
+        haskey(Pkg.installed(), "Revise") || @eval Pkg.add("Revise")
+    catch
     end
+    try
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+    catch
+    end
+    =#
+    
+    
+    # Fix nightmare pasting into the windows repl (as of v1.1)
+    if isdefined(repl, :options)
+        repl.options = REPL.Options(auto_indent=false) 
+    end
+
+     # Printing function for arrays
+    @eval showall(array) = show(IOContext(stdout, :limit=>false), MIME"text/plain"(), array)
 end
