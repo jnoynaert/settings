a1cf084738af4a0dd374cf9501b2aa37f8169cf9
Jared <jmnoynaert@gmail.com>
2020-03-31 22:36:16 -0500

    add git shortcut

--- git/git-config.sh
+++ git/git-config.sh
@@ -3,4 +3,5 @@
 git config --global user.name "Jared"
 git config --global user.email "jmnoynaert@gmail.com"
 git config --global alias.adog "log --all --decorate --oneline --graph"
+git config --global alias.dog "log --decorate --oneline --graph"
 git config --global alias.history "log --graph --abbrev-commit --decorate --date=relative --all"
