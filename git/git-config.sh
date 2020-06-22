073e35dc5db8ab1939c5f8f012f21931f6932f97
Jared <jmnoynaert@gmail.com>
2020-06-22 13:48:52 -0500

    update 22 June 2020

--- git/git-config.sh
+++ git/git-config.sh
@@ -6,3 +6,6 @@ git config --global alias.adog "log --all --decorate --oneline --graph"
 git config --global alias.dog "log --decorate --oneline --graph"
 git config --global alias.history "log --graph --abbrev-commit --decorate --date=relative --all"
 git config --global alias.amend "commit --amend"
+git config --global alias.tag "tag -a"
+git config --global alias.merge "merge --no-ff"
+git config --global push.followTags true
